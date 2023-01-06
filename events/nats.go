2016f65dce1afa9d402ac4c71b435f8449c102f3
germansanz93 <germansanz93@gmail.com>
2023-01-06 09:06:48 -0300

    continue implementing nats

--- events/nats.go
+++ events/nats.go
@@ -39,6 +39,36 @@ func (n *NatsEventStore) PublishCreatedFeed(ctx context.Context, feed *models.Fe
 	return n.conn.Publish(msg.Type(), data)
 }
 
+func (n *NatsEventStore) OnCreateFeed(f func(CreatedFeedMessage)) (err error) {
+	msg := CreatedFeedMessage{}
+	n.feedCreatedSub, err = n.conn.Subscribe(msg.Type(), func(m *nats.Msg) {
+		n.decodeMessage(m.Data, &msg)
+		f(msg)
+	})
+	return
+}
+
+func (n *NatsEventStore) SubscribeCreatedFeed(ctx context.Context) (<-chan CreatedFeedMessage, error) {
+	m := CreatedFeedMessage{}
+	n.feedCreatedChan = make(chan CreatedFeedMessage, 64)
+	ch := make(chan *nats.Msg, 64)
+	var err error
+	n.feedCreatedSub, err = n.conn.ChanSubscribe(m.Type(), ch)
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+		for {
+			select {
+			case msg := <-ch:
+				n.decodeMessage(msg.Data, &m)
+				n.feedCreatedChan <- m
+			}
+		}
+	}()
+	return (<-chan CreatedFeedMessage)(n.feedCreatedChan), nil
+}
+
 func (n *NatsEventStore) Close() {
 	if n.conn != nil {
 		n.conn.Close()
@@ -56,4 +86,10 @@ func (n *NatsEventStore) encodeMessage(m Message) ([]byte, error) {
 		return nil, err
 	}
 	return b.Bytes(), nil
+}
+
+func (n *NatsEventStore) decodeMessage(data []byte, m interface{}) error {
+	b := bytes.Buffer{}
+	b.Write(data)
+	return gob.NewDecoder(&b).Decode(m)
 }
