1db84ce1caf11969729eb923beff61c841fca080
germansanz93 <germansanz93@gmail.com>
2023-01-08 15:59:25 -0300

    nginx

--- pusher-service/helper.go
+++ pusher-service/helper.go
@@ -7,7 +7,7 @@ type CreatedFeedMessage struct {
 	ID          string    `json:"id"`
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
-	CreatedAt   time.Time `json:"created_at`
+	CreatedAt   time.Time `json:"created_at"`
 }
 
 func newCreatedFeedMessage(id, title, description string, createdAt time.Time) *CreatedFeedMessage {
